94f61f2f74ca0872aba46001755c55d79ee9e557
OckJuWon0831 <ockjuwon99@naver.com>
2023-01-03 23:56:46 +0900

    CSS: add transparent class
    To add/remove easily

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-const navbarHeight = document.querySelector("#navbar").scrollHeight;
 const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
 
 // Used scrollTop and window scroll event 
 window.addEventListener("scroll", () => {
     const stOriginal = 0;
     const st = Math.floor(document.documentElement.scrollTop);
     if (st > stOriginal && st >= navbarHeight){
-        navbar.style.opacity = '0.8';
+        navbar.classList.add("navbar--transparent--off");
     } else {
-        navbar.removeAttribute("style");
+        navbar.classList.remove("navbar--transparent--off");
     }
 });
