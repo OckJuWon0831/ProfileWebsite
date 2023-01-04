eb1340d78d11035c3054573f142b989a14507346
OckJuWon0831 <ockjuwon99@naver.com>
2023-01-04 14:02:27 +0900

    JS: window.scrollY update

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
+// // Used scrollTop and window scroll event 
+// window.addEventListener("scroll", () => {
+//     const stOriginal = 0;
+//     const st = Math.floor(document.documentElement.scrollTop);
+//     if (st > stOriginal && st >= navbarHeight){
+//         navbar.classList.add("navbar--transparent--off");
+//     } else {
+//         navbar.classList.remove("navbar--transparent--off");
+//     }
+// });
+
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
-
-// Used scrollTop and window scroll event 
 window.addEventListener("scroll", () => {
-    const stOriginal = 0;
-    const st = Math.floor(document.documentElement.scrollTop);
-    if (st > stOriginal && st >= navbarHeight){
+    if (window.scrollY > navbarHeight){
         navbar.classList.add("navbar--transparent--off");
     } else {
         navbar.classList.remove("navbar--transparent--off");
